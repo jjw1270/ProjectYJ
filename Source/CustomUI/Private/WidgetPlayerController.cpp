ea33fe3ed2f3e6a9f504ec51af02931a647db204
YunjeJang <jjw1270@naver.com>
2026-04-24 01:45:29 +0900

    widget player controller : 초기 Input Mode setting

--- Source/CustomUI/Private/WidgetPlayerController.cpp
+++ Source/CustomUI/Private/WidgetPlayerController.cpp
@@ -3,3 +3,40 @@
 
 #include "WidgetPlayerController.h"
 
+void AWidgetPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	switch (_InitialInputMode)
+	{
+	case EInputMode::GameAndUI:
+	{
+		FInputModeGameAndUI input_mode;
+		input_mode.SetLockMouseToViewportBehavior(_InMouseLockMode);
+		input_mode.SetHideCursorDuringCapture(_HideCursorDuringCapture);
+		SetInputMode(input_mode);
+	}
+		break;
+
+	case EInputMode::GameOnly:
+	{
+		FInputModeGameOnly input_mode;
+		input_mode.SetConsumeCaptureMouseDown(_ConsumeCaptureMouseDown);
+		SetInputMode(input_mode);
+	}
+		break;
+
+	case EInputMode::UIOnly:
+		{
+			FInputModeUIOnly input_mode;
+			input_mode.SetLockMouseToViewportBehavior(_InMouseLockMode);
+			SetInputMode(input_mode);
+		}
+		break;
+
+	default:
+		break;
+	}
+
+	SetShowMouseCursor(_ShowMouseCursor);
+}
--- Source/CustomUI/Public/WidgetPlayerController.h
+++ Source/CustomUI/Public/WidgetPlayerController.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "WidgetDefines.h"
 #include "WidgetPlayerController.generated.h"
 
 class UPageBase;
@@ -12,6 +13,25 @@ UCLASS(Abstract)
 class CUSTOMUI_API AWidgetPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "InputMode")
+	EInputMode _InitialInputMode = EInputMode::GameOnly;
+
+	UPROPERTY(EditAnywhere, Category = "InputMode", meta = (EditCondition = "_InitialInputMode==EInputMode::UIOnly || _InitialInputMode==EInputMode::GameAndUI"))
+	EMouseLockMode _InMouseLockMode = EMouseLockMode::DoNotLock;
+
+	UPROPERTY(EditAnywhere, Category = "InputMode", meta = (EditCondition = "_InitialInputMode==EInputMode::GameAndUI"))
+	bool _HideCursorDuringCapture = true;
+
+	UPROPERTY(EditAnywhere, Category = "InputMode", meta = (EditCondition = "_InitialInputMode==EInputMode::GameOnly"))
+	bool _ConsumeCaptureMouseDown = false;
+
+	UPROPERTY(EditAnywhere)
+	bool _ShowMouseCursor = true;
+
+protected:
+	virtual void BeginPlay() override;
 	
 protected:
 	UPROPERTY(EditAnywhere, meta = (DeprecatedProperty, DeprecationMessage = "Page는 이제 안쓰는 개념입니다."))
